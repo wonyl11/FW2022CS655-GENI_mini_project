2782891e2b6c2b1ca8a2b293a59f8ad7ccd7b1b6
wonyl11 <113034755+wonyl11@users.noreply.github.com>
2022-12-08 21:54:53 -0500

    Update update_python.sh

--- update_python.sh
+++ update_python.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+wget https://raw.githubusercontent.com/wonyl11/FW2022CS655-GENI_mini_project/main/code/Client.py
+wget https://raw.githubusercontent.com/wonyl11/FW2022CS655-GENI_mini_project/main/code/client_params
+
+wget https://raw.githubusercontent.com/wonyl11/FW2022CS655-GENI_mini_project/main/code/Institution.py
+wget https://raw.githubusercontent.com/wonyl11/FW2022CS655-GENI_mini_project/main/code/institution_params
+
+wget https://raw.githubusercontent.com/wonyl11/FW2022CS655-GENI_mini_project/main/code/ParameterServer.py
+wget https://raw.githubusercontent.com/wonyl11/FW2022CS655-GENI_mini_project/main/code/paramsvr_params
+
+wget https://raw.githubusercontent.com/wonyl11/FW2022CS655-GENI_mini_project/main/requirements.txt
+
 add-apt-repository ppa:deadsnakes/ppa
 apt update
-apt install python3.10
+apt-get install python3.10
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
-wget https://bootstrap.pypa.io/get-pip.py
+
 sudo apt-get remove --purge python-pip
-python3 get-pip.py
+apt install python3-pip
 pip3 install -r requirements.txt
